7aac5add4622af69397c408f3f5baaae4fba41f4
jjjjjwon <jjjjjwon21@gmail.com>
2022-09-27 22:05:09 +0900

    first commit

---
+++ 07 사칙연산/App.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\n')[0].split(' ').map(Number);
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0].split(' ').map(Number);
+
+let a = input[0];
+let b = input[1];
+console.log(`${a+b}\n${a-b}\n${a*b}\n${Math.floor(a/b)}\n${a%b}`);
+
+// let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\n')[0];
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0];
+
+// let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\r\n');
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\r\n');
---
+++ 08 ??!/App.js
@@ -0,0 +1,12 @@
+let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\n')[0]
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"})
+
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0].split(' ').map(Number);
+
+console.log(input+"??!");
+
+// let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\n')[0];
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0];
+
+// let input = require('fs').readFileSync(__dirname + '/input.txt',{encoding:"utf-8"}).split('\r\n');
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\r\n');
