2e1a14ea4abcf6a6a2a9ff3d6a92c8ce02175b73
jjjjjwon <jjjjjwon21@gmail.com>
2022-10-12 23:30:45 +0900

    1차원 배열...

--- 4 1차원 배열/3052 나머지/App.js
+++ 4 1차원 배열/3052 나머지/App.js
@@ -5,7 +5,16 @@ let input = require("fs").readFileSync(__dirname + "/input.txt", { encoding: "ut
 // let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0].split(' ').map(Number);
 
 console.log(input);
-
+let count =0;
 for(let i =0; i<input.length; i++){
-    console.log(input[i]%42);
+    // console.log(input.indexOf(input[i]%42)==input[i]%42);
+    for(let j =0; j<input.length; j++){
+        // console.log(input.indexOf(input[i]%42))
+        if(input[i]%42 == input[j]%42)
+        count++;
+    }
+    
+    // if(input[i]%42)
+    //     count ++;
+    console.log(count);
 }
