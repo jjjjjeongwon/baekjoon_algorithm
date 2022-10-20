ddd83ba6fc85f2f8e94c413c63540c72925106f6
jjjjjwon <jjjjjwon21@gmail.com>
2022-10-20 23:53:27 +0900

    1차원 배열 해결중

--- 4 1차원 배열/3052 나머지/App.js
+++ 4 1차원 배열/3052 나머지/App.js
@@ -6,15 +6,9 @@ let input = require("fs").readFileSync(__dirname + "/input.txt", { encoding: "ut
 
 console.log(input);
 let count =0;
+let arr = 0;
 for(let i =0; i<input.length; i++){
-    // console.log(input.indexOf(input[i]%42)==input[i]%42);
-    for(let j =0; j<input.length; j++){
-        // console.log(input.indexOf(input[i]%42))
-        if(input[i]%42 == input[j]%42)
-        count++;
-    }
-    
-    // if(input[i]%42)
-    //     count ++;
-    console.log(count);
+arr=input[i]%42;
+console.log(arr.split("\n").map(Number));
 }
+
