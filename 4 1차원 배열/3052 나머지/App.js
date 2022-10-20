1d2b5d6ab0981cadc2ca6f6827d1cd4003efc1b2
jjjjjwon <jjjjjwon21@gmail.com>
2022-10-21 01:34:08 +0900

    1차원 배열

--- 4 1차원 배열/3052 나머지/App.js
+++ 4 1차원 배열/3052 나머지/App.js
@@ -4,11 +4,14 @@ let input = require("fs").readFileSync(__dirname + "/input.txt", { encoding: "ut
 
 // let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0].split(' ').map(Number);
 
-console.log(input);
-let count =0;
-let arr = 0;
-for(let i =0; i<input.length; i++){
-arr=input[i]%42;
-console.log(arr.split("\n").map(Number));
-}
+// const fs =require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(x => Number(x));
 
+//입력으로 받은 요소 각각에 42로 나눈 나머지를 새로운 배열에 저장해준다.
+const newInput = input.map((x) => x%42);
+//새로운 배열 중 중복을 제거한다.
+const set = new Set(newInput);
+const uniqueArr = [...set];
+
+//배열의 길이 = 나머지 개수
+console.log(uniqueArr.length);
