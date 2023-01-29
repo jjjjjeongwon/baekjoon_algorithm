43fee56ea7a8b241fa0af2dd9b38a2c3fd442201
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-01-29 23:57:28 +0900

    complete 백준 나머지 for,if/for,forEach 사용 풀이

--- 4 1차원 배열/3052 나머지/App.js
+++ 4 1차원 배열/3052 나머지/App.js
@@ -1,14 +1,35 @@
-let input = require("fs").readFileSync(__dirname + "/input.txt", { encoding: "utf-8" }).split("\n").map(Number);
+let input = require('fs')
+  .readFileSync(__dirname + '/input.txt', { encoding: 'utf-8' })
+  .split('\n')
+  .map(Number);
 // let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n').map(Number);
 // let input = require('fs').readFileSync(0,{encoding:"utf-8"})
 
 // let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n')[0].split(' ').map(Number);
 
-console.log(input);
-let count =0;
-let arr = [];
-for(let i =0; i<input.length; i++){
-console.log((input[i])%42);
-// console.log(arr.split("\n").map(Number));
+const arr = [];
+
+for (let i = 0; i < 10; i++) {
+    const remain = input[i]%42;
+  if(!arr.includes(remain)){
+    arr.push(remain);
+  }
 }
+
+console.log(arr.length);
+
+//2안
+// const arr = [];
+// const result = {};
+
+// for (let i = 0; i < input.length; i++) {
+//   arr.push(input[i] % 42);
+// }
+
+// arr.forEach((x) => {
+//   result[x] = (result[x] || 0) + 1;
+// });
+
+// console.log(Object.keys(result).length);
+
 
