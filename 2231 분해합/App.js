6256770983655bc63b126e311de6efe2c04b55c9
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-07-05 03:12:27 +0900

    complete

---
+++ 2231 분해합/App.js
@@ -0,0 +1,17 @@
+let input = require('fs')
+  .readFileSync(__dirname + '/input.txt', { encoding: 'utf-8' })
+  .split('\n')[0];
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"}).split('\n').map(Number);
+// let input = require('fs').readFileSync(0,{encoding:"utf-8"})
+
+let targetNumber = Number(input);
+
+for (let i = 0; i < 1000000; i++) {
+  let arr = String(i).split('').map(Number);
+  let sum = arr.reduce((a, b) => a + b, 0);
+
+  if (targetNumber === i + sum) {
+    return console.log(i);
+  }
+}
+return console.log(0);
