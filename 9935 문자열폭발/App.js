0387d258acc26919c256d05883af81a07061827f
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-09-09 21:03:20 +0900

    complete

--- 9935 문자열폭발/App.js
+++ 9935 문자열폭발/App.js
@@ -2,28 +2,20 @@ let input = require('fs')
   .readFileSync(__dirname + '/input.txt', { encoding: 'utf-8' })
   .split('\n');
 
-let wordList = input[0].split('');
-let bomb = input[1].split('');
-let wordStack = [];
+let wordList = input[0];
+let bomb = input[1];
+let bombLength = bomb.length;
 
-// console.log(wordList.length);
-while (true) {
-  for (let i = 0; i < wordList.length; i++) {
-    console.log(wordList[i], bomb[0], i);
-    if (wordList[i] !== bomb[0]) {
-      wordStack.push(wordList[i]);
-    } else {
-      console.log(i);
+let wordListStack = [];
 
-      for (let j = i; j < i + bomb.length; j++) {
-        console.log('check', j, wordList[j], bomb[j - i]);
-        if (wordList[j] !== bomb[j - i]) {
-          wordStack.push(wordList[j]);
-        }
-      }
+for (let i = 0; i < wordList.length; i++) {
+  wordListStack.push(wordList[i]);
+  if (wordList[i] === bomb[bombLength - 1]) {
+    let check = wordListStack.slice(-bombLength).join('');
+    if (check === bomb) {
+      wordListStack.splice(wordListStack.length - bombLength, bombLength);
     }
   }
-  console.log('checkWordStack', wordStack);
-  wordList = wordStack;
-  console.log('wordList', wordList);
 }
+
+console.log(wordListStack.join('') ? wordListStack.join('') : 'FRULA');
