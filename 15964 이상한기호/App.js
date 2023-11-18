dcd11f5049ffef63524997304061040e8e356a25
jjjjjeongwon <jjjjjwon21@gmail.com>
2023-11-18 23:58:23 +0900

    complete

---
+++ 15964 이상한기호/App.js
@@ -0,0 +1,11 @@
+let input = require("fs")
+  .readFileSync(__dirname + "/input.txt", {
+    encoding: "utf-8",
+  })
+  .split("\n")[0]
+  .split(" ")
+  .map(Number);
+
+const [A, B] = input;
+
+console.log(A, B);
